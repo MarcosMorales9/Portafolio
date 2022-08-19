9d405f3b7bf43b3480874066f543ca9ee5c89cec
Marcos <marcosmorales9892@gmail.com>
2022-08-18 22:09:17 -0300

    data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects =[
+    {
+        id:1,
+        title: "My Industrial Home",
+        description: "React,Node,Express, Redux, Mongo",
+        image:"https://media.discordapp.net/attachments/821196906814111765/1007108594006765658/unknown.png?width=1335&height=676",
+        page: "https://my-industrial-home-challange-mind-hub.vercel.app",
+        codegit:"https://github.com/MarcosMorales9/my-industrial-home-challangeMindHub"
+    },
+    {
+        id:2,
+        title: "MyTinerary",
+        description: "MERN",
+        image:"https://media.discordapp.net/attachments/821196906814111765/1009467212689588384/unknown.png?width=1328&height=676",
+        page: "https://mytinerary-morales-front.herokuapp.com",
+        codegit:"https://github.com/MarcosMorales9/MyTinerary-Morales"
+    },
+    {
+        id:3,
+        title: "Amazing Events",
+        description: "HTML,CSS,JAVASCRIPT",
+        image:"https://media.discordapp.net/attachments/821196906814111765/1009468098342039672/unknown.png?width=1324&height=676",
+        page: "https://marcosmorales9.github.io/AmazingEvents",
+        codegit:"https://github.com/MarcosMorales9/AmazingEvents"
+    },
+    {
+        id:4,
+        title: "MyTinerary Mobile",
+        description: "React Native, Mobile",
+        image:"https://media.discordapp.net/attachments/821196906814111765/1009468098342039672/unknown.png?width=1324&height=676",
+        page: "https://github.com/MarcosMorales9/myTinerary",
+        codegit:"https://github.com/MarcosMorales9/AmazingEvents"
+    },
+]
