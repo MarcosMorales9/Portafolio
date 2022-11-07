13151ed62cb13549e6f7725e79f3b20fd9efba01
Marcos <marcosmorales9892@gmail.com>
2022-11-06 23:13:54 -0300

    optimizar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import AboutMe from "./components/AboutMe";
 import Proyects from "./components/Proyects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import {makeStyles} from "@material-ui/core"
 import "./App.css";
 
 function App() {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
+    <div>
       <Navbar  />
       <AboutMe id="about" tittle="Acerca de mi" dark={true} />
       <Proyects id="proyects" tittle="Proyectos" dark={true}/>
@@ -18,11 +16,5 @@ function App() {
     </div>
   );
 }
-
-const useStyles = makeStyles((theme)=> ({
-  root:{
-    
-  }
-}))
 
 export default App;
